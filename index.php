06726e1992201d95ff4d04b35ea0bcade805ac26
Yuhai Artem <tema3112002@gmail.com>
2023-10-06 00:35:29 +0900

    InnoReef

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $temperature = isset($_POST['temperature']) ? $_POST['temperature'] : null;
     $humidity = isset($_POST['humidity']) ? $_POST['humidity'] : null;
+    $waterLevel = isset($_POST['waterLevel']) ? $_POST['waterLevel'] : null; // Добавляем уровень воды
 
-    
     $servername = "localhost";
     $username = "icoral";
     $password = "wjqthr12!";
@@ -15,9 +15,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "INSERT INTO sensor_data (temperature, humidity) VALUES (?, ?)";
+    $sql = "INSERT INTO sensor_data (temperature, humidity, waterLevel) VALUES (?, ?, ?)"; // Обновляем запрос SQL
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("dd", $temperature, $humidity);
+    $stmt->bind_param("ddd", $temperature, $humidity, $waterLevel); // Обновляем параметры
 
     if ($stmt->execute() === TRUE) {
         echo "Data inserted successfully.";
@@ -33,55 +33,100 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            font-family: Arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
 
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
 
-    th, td {
-        border: 1px solid #ddd;
-        padding: 8px;
-        text-align: left;
-    }
+        th {
+            background-color: #f2f2f2;
+        }
 
-    th {
-        background-color: #f2f2f2;
-    }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
 
-    tr:nth-child(even) {
-        background-color: #f2f2f2;
-    }
+        tr:hover {
+            background-color: #ddd;
+        }
 
-    tr:hover {
-        background-color: #ddd;
-    }
-</style>
+        h1 {
+            text-align: center;
+        }
 
-<?php
-$servername = "localhost";
-$username = "icoral";
-$password = "wjqthr12!";
-$dbname = "icoral";
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+        .refresh-button {
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Sensor Data</h1>
+        <button class="refresh-button" onclick="refreshData()">Refresh Data</button>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Temperature</th>
+                <th>Humidity</th>
+                <th>Water Level</th>
+                <th>Timestamp</th>
+            </tr>
+            <?php
+            $servername = "localhost";
+            $username = "icoral";
+            $password = "wjqthr12!";
+            $dbname = "icoral";
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "SELECT * FROM sensor_data";
-$result = $conn->query($sql);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
 
-if ($result->num_rows > 0) {
-    echo "<table><tr><th>ID</th><th>Temperature</th><th>Humidity</th><th>Timestamp</th></tr>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["temperature"] . "</td><td>" . $row["humidity"] . "</td><td>" . $row["timestamp"] . "</td></tr>";
-    }
-    echo "</table>";
-} else {
-    echo "No data available";
-}
-$conn->close();
-?>
+            $sql = "SELECT * FROM sensor_data";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr><td>" . $row["id"] . "</td><td>" . $row["temperature"] . "</td><td>" . $row["humidity"] . "</td><td>" . $row["waterLevel"] . "</td><td>" . $row["timestamp"] . "</td></tr>";
+                }
+            } else {
+                echo "<tr><td colspan='5'>No data available</td></tr>";
+            }
+            $conn->close();
+            ?>
+        </table>
+    </div>
+    <script>
+        function refreshData() {
+            location.reload();
+        }
+    </script>
+</body>
+</html>
--- sketch_sep20a_copy_20230925134739_copy_20231004171637/sketch_sep20a_copy_20230925134739_copy_20231004171637.ino
+++ sketch_sep20a_copy_20230925134739_copy_20231004171637/sketch_sep20a_copy_20230925134739_copy_20231004171637.ino
@@ -4,6 +4,8 @@
 const char* ssid = "DLive_3852";
 const char* password = "E0012D3851";
 
+#define WATER_LEVEL_PIN A0
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -15,13 +17,18 @@ void setup() {
   Serial.println("");
   Serial.println("Wi-Fi connected");
 }
+
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
+    int waterLevel = analogRead(WATER_LEVEL_PIN); // Считываем уровень воды
+
     WiFiClient client;
     HTTPClient http;
     http.begin(client, "http://icoral.dothome.co.kr/index.php");
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    String postData = "temperature=25&humidity=50";
+
+    String postData = "temperature=25&humidity=50&waterLevel=" + String(waterLevel);
+
     int httpResponseCode = http.POST(postData);
     if (httpResponseCode > 0) {
       String response = http.getString();
@@ -34,5 +41,5 @@ void loop() {
     }
     http.end();
   }
-  delay(60000);
+  delay(60000 * 20);
 }
